d8f37759aa7ad564b99a00672d88b092c96c93ad
Lxrcade <167519963+Lxrcade@users.noreply.github.com>
2026-09-04 18:30:12 +0800

    fix: restore missing script setup tag in LandingView

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,3 +1,4 @@
+<script setup>
 import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { Music, ArrowRight, ShieldCheck, Calendar, Users, Cpu, Sun, Moon } from 'lucide-vue-next'
